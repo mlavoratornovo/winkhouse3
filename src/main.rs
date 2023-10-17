6514272138026b0e6afca2cbe53de1540aa367fd
Michele <m.lavoratornovo@gmail.com>
2023-10-17 20:51:58 +0200

    dioxus fullstack project init with tailwind css

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case, unused)]
+use dioxus::prelude::{*, GlobalAttributes};
+use dioxus_fullstack::prelude::*;
+
+fn main() {
+    LaunchBuilder::new(app).launch();
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
+    cx.render(rsx! {
+        h1 { "High-Five counter: {count}" }
+        button { class:"text-3xl font-bold underline",
+            onclick: move |_| count += 1, "Up high!",
+        }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    })
+}
+
